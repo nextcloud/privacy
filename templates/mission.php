6d2bbf7c8436bb5d70c00146e6a7a31e8db94d49
John Molakvoæ <skjnldsv@protonmail.com>
2022-01-07 15:51:37 +0100

    Update script/style to OCP\Util
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- templates/mission.php
+++ templates/mission.php
@@ -1,8 +1,3 @@
-<?php
-script('privacy', 'privacy-main');
-style('privacy', 'privacy');
-?>
-
 <div id="themes" class="section">
 	<h2><?php p($l->t('Your Nextcloud account')) ?></h2>
 	<p><?php p($l->t('Your data represents who you are and can easily be abused. We at Nextcloud believe that you have a right to decide what happens with your data. Nextcloud protects your rights as defined in the User Data Manifesto!')); ?></p>
