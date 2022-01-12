f2c628bd476c158e6aa3a82c6f2bbfcd09e7545c
Joas Schilling <coding@schilljs.com>
2022-01-12 10:19:33 +0100

    Fix label of account name
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- templates/mission.php
+++ templates/mission.php
@@ -4,6 +4,6 @@ style('privacy', 'privacy');
 ?>
 
 <div id="themes" class="section">
-	<h2><?php p($l->t('Your Nextcloud account')) ?></h2>
+	<h2><?php p($l->t('Your %s account', $theme->getName())) ?></h2>
 	<p><?php p($l->t('Your data represents who you are and can easily be abused. We at Nextcloud believe that you have a right to decide what happens with your data. Nextcloud protects your rights as defined in the User Data Manifesto!')); ?></p>
 </div>
