231e2ec8aac9f197c090ecba7e7b0b8ba8233a83
Christopher Ng <chrng8@gmail.com>
2023-11-24 15:03:00 -0800

    chore(dev): Add webpack serve script - Use with https://github.com/nextcloud/hmr_enabler
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- webpack.js
+++ webpack.js
@@ -1,3 +1,6 @@
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
+// Let webpack determine automatically where it's located
+webpackConfig.output.publicPath = 'auto'
+
 module.exports = webpackConfig
