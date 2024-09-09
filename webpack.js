bf683661c5a3634db4214e7a0a27314d43756493
Andy Scherzinger <info@andy-scherzinger.de>
2024-09-09 16:08:43 +0200

    chore: Migrate reuse to toml
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- webpack.js
+++ webpack.js
@@ -1,9 +1,42 @@
 const path = require('path')
+const WebpackSPDXPlugin = require('./build-js/WebpackSPDXPlugin.js')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+
+const isDev = process.env.NODE_ENV === 'development'
 
 webpackConfig.entry.main = path.join(__dirname, 'src', 'main.ts')
 
 // Let webpack determine automatically where it's located
 webpackConfig.output.publicPath = 'auto'
+
+// Generate reuse license files if not in development mode
+if (!isDev) {
+	webpackConfig.plugins.push(new WebpackSPDXPlugin({
+		override: {
+			select2: 'MIT',
+			'@nextcloud/axios': 'GPL-3.0-or-later',
+			'@nextcloud/vue': 'AGPL-3.0-or-later',
+			'nextcloud-vue-collections': 'AGPL-3.0-or-later',
+		},
+	}))
+
+	webpackConfig.optimization.minimizer = [{
+		apply: (compiler) => {
+			// Lazy load the Terser plugin
+			const TerserPlugin = require('terser-webpack-plugin')
+			new TerserPlugin({
+				extractComments: false,
+				terserOptions: {
+					format: {
+						comments: false,
+					},
+					compress: {
+						passes: 2,
+					},
+				},
+		  }).apply(compiler)
+		},
+	}]
+}
 
 module.exports = webpackConfig
