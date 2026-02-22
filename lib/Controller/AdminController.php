77c09cd1f0580f75e5ab45b9629bdfa470cf919e
Josh <josh.t.richards@gmail.com>
2026-02-22 10:32:38 -0500

    chore: tidy up AdminController.php
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- lib/Controller/AdminController.php
+++ lib/Controller/AdminController.php
@@ -43,56 +43,51 @@ class AdminController extends Controller {
 	}
 
 	/**
-	 * @param string $code
-	 * @return JSONResponse
+	 * Sets the readable location code for the app.
 	 */
-	public function setReadableLocation(string $code):JSONResponse {
+	public function setReadableLocation(string $code): JSONResponse {
 		$this->config->setAppValue($this->appName, 'readableLocation', $code);
 		return new JSONResponse([], Http::STATUS_OK);
 	}
 
 	/**
-	 * @param string $name
-	 * @return JSONResponse
+	 * Adds an external privacy admin by display name.
 	 */
-	public function addAdditionalAdmin(string $name):JSONResponse {
-		$query = $this->dbConnection->getQueryBuilder();
-		$query->insert('privacy_admins')
-			->setValue('displayname', $query->createNamedParameter($name))
-			->execute();
+	public function addAdditionalAdmin(string $displayName): JSONResponse {
+		$qb = $this->dbConnection->getQueryBuilder();
+		$qb->insert('privacy_admins')
+			->setValue('displayname', $qb->createNamedParameter($displayName))
+			->executeStatement();
 
-		$id = $query->getLastInsertId();
+		$id = $qb->getLastInsertId();
 
 		return new JSONResponse([
 			'id' => $id,
-			'displayname' => $name,
+			'displayname' => $displayName,
 			'internal' => false,
 		], Http::STATUS_CREATED);
 	}
 
 	/**
-	 * @param int $id
-	 * @return JSONResponse
+	 * Removes an external privacy admin by ID.
 	 */
-	public function deleteAdditionalAdmin(int $id):JSONResponse {
-		$query = $this->dbConnection->getQueryBuilder();
-		$query->delete('privacy_admins')
-			->where($query->expr()->eq('id', $query->createNamedParameter($id)))
-			->execute();
+	public function deleteAdditionalAdmin(int $id): JSONResponse {
+		$qb = $this->dbConnection->getQueryBuilder();
+		$qb->delete('privacy_admins')
+			->where($query->expr()->eq('id', $qb->createNamedParameter($id)))
+			->executeStatement();
 
 		return new JSONResponse([], Http::STATUS_OK);
 	}
 
 	/**
-	 * @param string $enabled
-	 * @return JSONResponse
+	 * Enables or disables full disk encryption indicator (only) for privacy disclosure purposes.
 	 */
-	public function setFullDiskEncryption(string $enabled):JSONResponse {
+	public function setFullDiskEncryption(string $enabled): JSONResponse {
 		$allowedValues = ['0', '1'];
 		if (!\in_array($enabled, $allowedValues, true)) {
 			return new JSONResponse([], HTTP::STATUS_NOT_ACCEPTABLE);
 		}
-
 		$this->config->setAppValue('privacy', 'fullDiskEncryptionEnabled', $enabled);
 		return new JSONResponse([], HTTP::STATUS_OK);
 	}
